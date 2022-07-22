e29cac8dc562e74307a347465afdc2e3add38fad
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-22 20:16:29 -0300

    import readStream

--- example01-nodejs-common-scenarios/index.mjs
+++ example01-nodejs-common-scenarios/index.mjs
@@ -9,12 +9,14 @@
 // node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
 
 import http from "http"
-import { read, readFileSync } from "fs"
+import { read, readFileSync, createReadStream } from "fs"
 
 http.createServer((request, response) => {
-  const file = readFileSync('big.file').toString();
-  response.write(file)
-  response.end
+  // const file = readFileSync('big.file').toString();
+  // response.write(file)
+  // response.end
+  createReadStream('big.file')
+  .pipe(response)
 })
 .listen(3000)
 .on('listening', () => console.log('server is listening at 3000'))

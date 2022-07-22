4d1867074951d91fe345c2eba9ce2d9dd2a1db5d
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-22 20:49:59 -0300

    append to log file

--- example02-creating-web-api/src/client.js
+++ example02-creating-web-api/src/client.js
@@ -1,4 +1,9 @@
+import { ChildProcess } from 'node:child_process'
 import { get } from 'node:http'
+import { createWriteStream } from 'node:fs'
+
+import { Transform, Writable } from 'node:stream'
+
 const url = "http://localhost:3000"
 
 const getHttpStream = () => new Promise(resolve => get(url, response => resolve(response)))
@@ -6,4 +11,33 @@ const getHttpStream = () => new Promise(resolve => get(url, response => resolve(
 const stream = await getHttpStream()
 
 stream
-  .pipe(process.stdout)
+  .pipe(
+    Transform({
+      objectMode: true,
+      transform(chunk, end, cb) {
+        const item = JSON.parse(chunk)
+        // console.log('chunk', JSON.parse(chunk))
+        const myNumber = /\d+/.exec(item.name)[0]
+        const isEven = myNumber % 2 === 0
+
+        item.name = item.name.concat(isEven ? ' is even' : ' is odd')
+
+        cb(null, JSON.stringify(item))
+      }
+    })
+  )
+  .filter(chunk => chunk.includes('even'))
+  .map(chunk => chunk.toUpperCase() + '\n')
+  .pipe(
+    createWriteStream('response.log', { flags: 'a' })
+  )
+  // .pipe(
+  //   Writable({
+  //     objectMode: true,
+  //     write(chunk, enc, cb) {
+  //       console.log('chunk', chunk);
+  //       return cb();
+  //     }
+  //   })
+  // )
+  // .pipe(process.stdout)

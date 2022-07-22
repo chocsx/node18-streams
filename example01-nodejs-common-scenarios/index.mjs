53587e8c54afd694293b2669b580ee1eb54e0643
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-22 20:13:27 -0300

    cannot read big string

--- example01-nodejs-common-scenarios/index.mjs
+++ example01-nodejs-common-scenarios/index.mjs
@@ -1,8 +1,20 @@
+// const stdin = process.stdin
+//   .on('data', msg => console.log('terminal input was', msg))
 
-const stdin = process.stdin
-  .on('data', msg => console.log('terminal input was', msg))
+// const stdout = process.stdout
+//   .on('data', msg => process.stdout.write(msg.toString().toUpperCase()))
 
-const stdout = process.stdout
-  .on('data', msg => process.stdout.write(msg.toString().toUpperCase()))
+// stdin.pipe(stdout)
 
-stdin.pipe(stdout)
+// node -e "process.stdout.write(crypto.randomBytes(1e9))" > big.file
+
+import http from "http"
+import { read, readFileSync } from "fs"
+
+http.createServer((request, response) => {
+  const file = readFileSync('big.file').toString();
+  response.write(file)
+  response.end
+})
+.listen(3000)
+.on('listening', () => console.log('server is listening at 3000'))
